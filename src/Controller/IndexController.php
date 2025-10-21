3e2de7fb36e26eb1f5c7f94863857759182e1641
Romain Gautier <mail@romain.sh>
2025-10-22 01:16:52 +0200

    add basic form

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\FooType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,5 +14,19 @@ final class IndexController extends AbstractController
     public function index(): Response
     {
         return $this->render('index.html.twig');
+    }
+
+    #[Route('/form', name: 'app_form')]
+    public function form(Request $request): Response
+    {
+        $form = $this->createForm(FooType::class);
+
+        if ($form->handleRequest($request)->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('app_index', ['form' => 'ok']);
+        }
+
+        return $this->render('form.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
