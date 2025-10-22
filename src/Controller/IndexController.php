53a1d7ee83fd036e843e8f425a9f189abfb16d9b
Romain Gautier <mail@romain.sh>
2025-10-22 10:56:06 +0200

    show ips

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Form\FooType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
@@ -17,7 +19,7 @@ final class IndexController extends AbstractController
     }
 
     #[Route('/form', name: 'app_form')]
-    public function form(Request $request): Response
+    public function form(Request $request, #[Autowire(env: 'SYMFONY_TRUSTED_PROXIES')] string $ips): Response
     {
         $form = $this->createForm(FooType::class);
 
@@ -26,6 +28,7 @@ final class IndexController extends AbstractController
         }
 
         return $this->render('form.html.twig', [
+            'ips' => $ips,
             'form' => $form,
             'errors' => $form->getErrors(),
         ]);
